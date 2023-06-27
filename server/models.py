407ec5eb5a2a3039027fdcb1c667503a0405b04e
Dennis Musembi <dennismusembi344@gmail.com>
2023-06-27 10:27:15 +0300

    done

--- server/models.py
+++ server/models.py
@@ -8,10 +8,28 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        # Validate that all authors have a name
+        if not name:
+            raise ValueError("Author must have a name")
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        # Validate that author phone numbers are exactly ten digits
+        if phone_number and len(phone_number) != 10:
+            raise ValueError("Author phone number must be exactly ten digits")
+        return phone_number
+    def validate_unique_name(self, key, name):
+        # Validate that no two authors have the same name
+        existing_author = Author.query.filter(Author.name == name).first()
+        if existing_author and existing_author.id != self.id:
+            raise ValueError("Author name must be unique")
+        return name
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +45,33 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        # Validate that all posts have a title
+        if not title:
+            raise ValueError("Post must have a title")
+        return title
 
+    @validates('content')
+    def validate_content(self, key, content):
+        # Validate that post content is at least 250 characters long
+        if content and len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        # Validate that post summary is a maximum of 250 characters
+        if summary and len(summary) > 250:
+            raise ValueError("Post summary must be a maximum of 250 characters")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        # Validate that post category is either Fiction or Non-Fiction
+        valid_categories = ['Fiction', 'Non-Fiction']
+        if category not in valid_categories:
+            raise ValueError("Invalid post category")
+        return category
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
